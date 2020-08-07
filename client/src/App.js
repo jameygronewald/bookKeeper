270b3e28304bff538959e195872482155f2e31b3
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-07 06:30:22 -0700

    cleared out App.js and index.js; added components directory with Footer and Navbar component starts; added pages for Saved and Search; built basic Router in App.js with Navbar and Footer wrapping each page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Search from './pages/Search/Search';
+import Saved from './pages/Saved/Saved';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
 }
