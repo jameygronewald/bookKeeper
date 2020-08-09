cc2cb295fb7a936962d818fb5d948d0740b8b504
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-08 20:31:08 -0700

    moved useState hook, handleSuubmit, and HandleChange from Form to Search page and passed down to Form as props; wrote BookInfo component to display book info from API call on Search Page; conditionally rendering BookInfo and Save book Button on Search page

--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
-import Form from '../../components/Form/Form';
+import React, { useState } from "react";
+import Form from "../../components/Form/Form";
 import API from "../../utils/API";
+import BookInfo from "../../components/BookInfo/BookInfo";
+import Button from "../../components/Button/Button";
 
 const Search = () => {
-    
-    const [search, setSearch] = useState("");
-    
-    const handleSubmit = event => {
-        event.preventDefault();
-        API.getBook(search)
-          .then(response => {
-            const bookInfo = response.data.items[0].volumeInfo;
-            const bookObj = {
-              title: bookInfo.title,
-              author: bookInfo.authors[0],
-              description: bookInfo.description,
-              cover: bookInfo.imageLinks.thumbnail,
-            };
-            console.log(bookObj);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      };
-    
-    return (
-        <div>
-            <Form setSearch={setSearch} handleSubmit={handleSubmit}/>
-        </div>
-    );
+  const [search, setSearch] = useState('');
+  const [book, setBook] = useState({});
+
+  const handleChange = ({ target: { value } }) => {
+    setSearch(value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    API.getBook(search)
+      .then(response => {
+        const bookInfo = response.data.items[0].volumeInfo;
+        setBook({
+          title: bookInfo.title,
+          author: bookInfo.authors[0],
+          description: bookInfo.description,
+          cover: bookInfo.imageLinks.thumbnail,
+        });
+        setSearch("");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <Form handleChange={handleChange} handleSubmit={handleSubmit} />
+      {book.title && <BookInfo book={book} />}
+      {book.title && <Button buttonText="Save book" />}
+    </div>
+  );
 };
 
 export default Search;
