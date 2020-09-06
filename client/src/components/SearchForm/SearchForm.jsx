e8d2ab28d8be18b25d66ad28c691da892bd24665
Jamey Gronewald <jrgronewald@gmail.com>
2020-09-05 21:39:44 -0700

    restructured Search.jsx into grid layout and fixed layout for search form

--- client/src/components/SearchForm/SearchForm.jsx
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 
-const SearchForm = ({ handleChange, handleSubmit }) => {
+const SearchForm = ({ handleChange, handleSubmit, className}) => {
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={className} onSubmit={handleSubmit}>
       <Input
         type="text"
         placeholder="Enter a book title"
